0cf62318507a0bbcc7001bc2ef52228c6ae0687c
Eoin Reid <40269943+EoinReid@users.noreply.github.com>
2022-12-21 21:38:34 +0000

    allows for dynamic source, dest and file extension
    adds code to allow for source directory, destination directory, and file extension type to be specified in the command line argument instead of needing to change the code

--- file-movers.py
+++ file-movers.py
@@ -1,27 +1,29 @@
 # File Mover script
-# just update the destination directory name and the file extension to move chosen files to chosen directory
-# e.g. change mv_files escond paramter from *.txt to *.png for png files
-# update destination from home/user/Text to home/user/Png
 import shutil
 import glob
 import os
 from pathlib import Path
+import sys
 
-def move_files(source, destination):
-    mv_files = glob.glob(os.path.join(source, '*.py'), recursive=True)
+def move_files(source, destination, file_ext):
+    mv_files = glob.glob(os.path.join(source, f'*{file_ext}'), recursive=True)
     for file in mv_files:
             dst_path = os.path.join(destination, os.path.basename(file))
             shutil.move(file, dst_path)
             print(f"Moved {file} -> {dst_path}")
-
 try:
-    source = Path.home()
-    destination = Path(Path.home() / "Python")
+    print(str(len(sys.argv)))
+    if len(sys.argv) < 4:
+        print("Usage: python3 file-movers.py [source] [destination] [file extension]" )
+        sys.exit()
+    elif len(sys.argv) == 4:
+        source = sys.argv[1]
+        destination = Path(sys.argv[2])
+        file_ext = sys.argv[3]
     if os.path.exists(destination):
-       move_files(source,destination)
+       move_files(source,destination,file_ext)
     else:
            os.mkdir(destination)
            move_files(source,destination)
-
 except FileNotFoundError:
     print("file or directory not found.")
